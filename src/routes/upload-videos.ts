a5571fc52f9fb33ab027bb89fe1a2c2f7c7c8a3a
diego <dmarques1508@gmail.com>
2023-12-18 13:56:24 -0300

    add: rotas upload, create transcription

--- src/routes/upload-videos.ts
+++ src/routes/upload-videos.ts
@@ -2,12 +2,17 @@ import { FastifyInstance } from "fastify";
 import { fastifyMultipart } from "@fastify/multipart";
 import path from "node:path";
 import { randomUUID } from 'node:crypto'
+import fs from 'node:fs'
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
 import { prisma } from "../lib/prisma";
+
+const pump = promisify(pipeline)
 
 export async function uploadVideoRoute(app: FastifyInstance) {
     app.register(fastifyMultipart, {
         limits: {
-            fileSize: 1_048_576 * 25,
+            fileSize: 1_048_576 * 75,
         }
     })
 
@@ -25,7 +30,21 @@ export async function uploadVideoRoute(app: FastifyInstance) {
         }
 
         const fileBaseName = path.basename(data.filename, extension)
+        const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
+        const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
 
+        await pump(data.file, fs.createWriteStream(uploadDestination))
+
+        const video = await prisma.video.create({
+            data: {
+                name: data.filename,
+                path: uploadDestination,
+            }
+        })
+
+        return {
+            video,
+        }
     })
 
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { fastify } from 'fastify'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-videos'
+import { createTrasncripitonRoute } from './routes/create-transcription'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTrasncripitonRoute)
 
 app.listen({
     port: 3333,
